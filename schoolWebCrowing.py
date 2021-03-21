f28924e06a385a4d752fc375ef1384178fcf8332
EeeUnS <euns312510@gmail.com>
2021-03-22 02:00:14 +0900

    Update schoolWebCrowing.py

--- schoolWebCrowing.py
+++ schoolWebCrowing.py
@@ -17,25 +17,32 @@ urlPK = "http://www.pknu.ac.kr"
 
 prelistPK = []
 prelistIT = []
+prelistCS = []
 
 def timed_job():
     global prelistPK
     global prelistIT
+    global prelistCS
     # contents > div.contents-inner > form:nth-child(3) > table > tbody > tr:nth-child(5) > td.title > a
     #while True:
     try:
         time.sleep(3600)
         # webhook.send("hellohello")
         responseIT = requests.get(urlIT + '/itcae/view.do?no=9576', verify=False)
+        responseCS = requests.get(urlIT + '/ced/view.do?no=11084', verify=False)
         responsePK = requests.get(urlPK + '/usrBoardActn.do?p_bm_idx=5&p_boardcode=PK10000005')
 
         listIT = []
         listPK = []
+        listCS = []
 
         while True:
             while responseIT.status_code != 200:
                 time.sleep(10)
                 responseIT = requests.get(urlIT + '/itcae/view.do?no=9576', verify=False)
+            while responseCS.status_code != 200:
+                time.sleep(10)
+                responseIT = requests.get(urlIT + '/ced/view.do?no=11084', verify=False)
             while responsePK.status_code != 200:
                 time.sleep(10)
                 responsePK = requests.get(urlPK + '/usrBoardActn.do?p_bm_idx=5&p_boardcode=PK10000005')
@@ -43,6 +50,10 @@ def timed_job():
             htmlIT = responseIT.text
             soupIT = BeautifulSoup(htmlIT, features='html.parser')
             listIT = soupIT.select(' #board_list > li > a ')
+
+            htmlCS = responseCS.text
+            soupCS = BeautifulSoup(htmlCS, features='html.parser')
+            listCS = soupCS.select(' #board_list > li > a ')
 
             # for i in range(len(listIT)):
             #     print( listIT[i].find('h4').text.strip() + '\n' + urlIT + listIT[i].attrs['href'])
@@ -52,7 +63,7 @@ def timed_job():
             listPK = soupPK.select('#contents > div.contents-inner > form > table > tbody > tr > td.title')
             # listPKtitle = soupPK.select('#contents > div.contents-inner > form > table > tbody > tr > td.title > a')
             #    print(listPK)
-            if listPK == [] or listIT == []:
+            if listPK == [] or listIT == [] or listCS == []:
                 print("break this")
                 continue
             break
@@ -69,6 +80,17 @@ def timed_job():
                     break
             if isIn == False:
                 differrentIT.append(i)
+        
+        differrentCS = []
+        for i in listCS:
+            isIn = False
+            for j in prelistCS:
+                if (i.find('h4').text.strip() == j.find('h4').text.strip()):
+                    isIn = True
+                    break
+            if isIn == False:
+                differrentCS.append(i)
+
         differrentPk = []
         for i in listPK:
             isIn = False
@@ -84,13 +106,18 @@ def timed_job():
         for i in differrentIT:
             text =  'IT '+ i.find('h4').text.strip() + '\n' + urlIT + i.attrs['href']
             webhook.send(text)
+        for i in differrentCS:
+            text =  'CS '+ i.find('h4').text.strip() + '\n' + urlIT + i.attrs['href']
+            webhook.send(text)
         for i in differrentPk:
             text = i.find('a').text.strip() + '\n' + urlPK + i.find('a').attrs['href']
             webhook.send(text)
         print('end')
-        if listIT != [] and listPK != []:
+        if listIT != [] and listCS != [] and listPK != []:
             prelistIT = listIT
             prelistPK = listPK
+            prelistCS = listCS
+
     except Exception as e:
         print(e)
         webhook.send(e)
@@ -137,6 +164,5 @@ sched = BlockingScheduler()
 sched.add_job(timed_job,'interval', minutes=60)
 print("scheduled")
 sched.start()
-
 
 
