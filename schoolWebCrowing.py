282d9e4d5b4953f1e73978ad3638041aff269f09
EeeUnS <euns312510@gmail.com>
2021-03-22 02:31:54 +0900

    Update schoolWebCrowing.py

--- schoolWebCrowing.py
+++ schoolWebCrowing.py
@@ -11,38 +11,28 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 urlIT = "https://cms.pknu.ac.kr"
+urlCS = urlIT
 urlPK = "http://www.pknu.ac.kr"
-
 #urlIT+'/itcae/view.do?no=9576'
 
 prelistPK = []
 prelistIT = []
 prelistCS = []
 
-def timed_job():
-    global prelistPK
-    global prelistIT
-    global prelistCS
-    # contents > div.contents-inner > form:nth-child(3) > table > tbody > tr:nth-child(5) > td.title > a
-    #while True:
-    try:
-        time.sleep(3600)
-        # webhook.send("hellohello")
-        responseIT = requests.get(urlIT + '/itcae/view.do?no=9576', verify=False)
-        responseCS = requests.get(urlIT + '/ced/view.do?no=11084', verify=False)
-        responsePK = requests.get(urlPK + '/usrBoardActn.do?p_bm_idx=5&p_boardcode=PK10000005')
 
-        listIT = []
-        listPK = []
-        listCS = []
+### get list
+def updateList(listIT, listCS, listPK)
+    while True:
+            responseIT = requests.get(urlIT + '/itcae/view.do?no=9576', verify=False)
+            responseCS = requests.get(urlCS + '/ced/view.do?no=11084', verify=False)
+            responsePK = requests.get(urlPK + '/usrBoardActn.do?p_bm_idx=5&p_boardcode=PK10000005')
 
-        while True:
             while responseIT.status_code != 200:
                 time.sleep(10)
                 responseIT = requests.get(urlIT + '/itcae/view.do?no=9576', verify=False)
             while responseCS.status_code != 200:
                 time.sleep(10)
-                responseIT = requests.get(urlIT + '/ced/view.do?no=11084', verify=False)
+                responseCS = requests.get(urlCS + '/ced/view.do?no=11084', verify=False)
             while responsePK.status_code != 200:
                 time.sleep(10)
                 responsePK = requests.get(urlPK + '/usrBoardActn.do?p_bm_idx=5&p_boardcode=PK10000005')
@@ -67,9 +57,25 @@ def timed_job():
                 print("break this")
                 continue
             break
-        # for i in range(len(listPK)):
+              # for i in range(len(listPK)):
         #     print(listPK[i].find('a').text.strip() + '\n' + urlPK + listPK[i].find('a')['href'])
         # - prelistIT
+
+def timed_job():
+    global prelistIT
+    global prelistCS
+    global prelistPK
+    # contents > div.contents-inner > form:nth-child(3) > table > tbody > tr:nth-child(5) > td.title > a
+    #while True:
+    try:
+        time.sleep(3600)
+        # webhook.send("hellohello")
+
+        listIT = []
+        listCS = []
+        listPK = []
+
+        updateList(listIT, listCS, listPK )
 
         differrentIT = []
         for i in listIT:
@@ -107,16 +113,17 @@ def timed_job():
             text =  'IT '+ i.find('h4').text.strip() + '\n' + urlIT + i.attrs['href']
             webhook.send(text)
         for i in differrentCS:
-            text =  'CS '+ i.find('h4').text.strip() + '\n' + urlIT + i.attrs['href']
+            text =  'CS '+ i.find('h4').text.strip() + '\n' + urlCS + i.attrs['href']
             webhook.send(text)
         for i in differrentPk:
             text = i.find('a').text.strip() + '\n' + urlPK + i.find('a').attrs['href']
             webhook.send(text)
         print('end')
+
         if listIT != [] and listCS != [] and listPK != []:
             prelistIT = listIT
-            prelistPK = listPK
             prelistCS = listCS
+            prelistPK = listPK
 
     except Exception as e:
         print(e)
@@ -125,51 +132,14 @@ def timed_job():
         # print(listPK[i].find('a').text.strip() + '\n' + urlPK + listPK[i].find('a')['href'])
         # listITtitle[i].text.strip() + '\n' + urlIT + listIT[i].attrs['href']
 
-
-
-while True:
-    responseIT = requests.get( urlIT+'/itcae/view.do?no=9576' , verify=False)
-    responseCS = requests.get(urlIT + '/ced/view.do?no=11084', verify=False)
-    responsePK = requests.get(urlPK + '/usrBoardActn.do?p_bm_idx=5&p_boardcode=PK10000005')
-
-    while responseIT.status_code != 200:
-        time.sleep(10)
-        responseIT = requests.get(urlIT+'/itcae/view.do?no=9576' , verify=False)
-
-    while responseCS.status_code != 200:
-        time.sleep(10)
-        responseIT = requests.get(urlIT + '/ced/view.do?no=11084', verify=False)
-
-
-    while responsePK.status_code != 200:
-        time.sleep(10)
-        responsePK = requests.get(urlPK+ '/usrBoardActn.do?p_bm_idx=5&p_boardcode=PK10000005')
-
-    htmlIT = responseIT.text
-    soupIT = BeautifulSoup(htmlIT, features='html.parser')
-    prelistIT = soupIT.select('#board_list > li > a')
-
-    htmlCS = responseCS.text
-    soupCS = BeautifulSoup(htmlCS, features='html.parser')
-    prelistCS = soupCS.select(' #board_list > li > a ')
-
-    htmlPK = responsePK.text
-    soupPK = BeautifulSoup(htmlPK, features='html.parser')
-    prelistPK = soupPK.select('#contents > div.contents-inner > form > table > tbody > tr > td.title')
-
-    if prelistPK == [] or prelistIT == [] or prelistCS == []:
-        print("break this")
-        continue
-    break
-
-
+########3 main###############################
+updateList(prelistIT, prelistCS, prelistPK)
 url = os.environ["WEBHOOKURL"]
 webhook = Webhook.from_url(url, adapter=RequestsWebhookAdapter())
 text = "hello"
 print(text)
 #webhook.send(text)
 #schedul
-
 sched = BlockingScheduler()
 sched.add_job(timed_job,'interval', minutes=60)
 print("scheduled")
